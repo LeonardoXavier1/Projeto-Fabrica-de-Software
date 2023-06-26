ab737924ba665c79c731810b44823d9baa89d418
LeonardoXavier1 <leonardomx500@gmail.com>
2023-06-25 22:30:20 -0300

    o

--- py/a.py
+++ py/a.py
@@ -1,16 +1,15 @@
 import pandas as pd
+# # Define o número máximo de linhas a serem lidas
+# max_linhas = 1
 
-# Define o número máximo de linhas a serem lidas
-max_linhas = 1
+# # Lê as primeiras 'max_linhas' linhas do arquivo CSV
+# data = pd.read_csv('2022_LoL_esports_match_data_from_OraclesElixir.csv', nrows=max_linhas)
 
-# Lê as primeiras 'max_linhas' linhas do arquivo CSV
-data = pd.read_csv('2022_LoL_esports_match_data_from_OraclesElixir.csv', nrows=max_linhas)
+# # Converte o DataFrame em JSON
+# json_data = data.to_json(orient='records')
 
-# Converte o DataFrame em JSON
-json_data = data.to_json(orient='records')
+# # Salva o JSON em um arquivo
+# with open('dados.json', 'w') as file:
+#     file.write(json_data)
 
-# Salva o JSON em um arquivo
-with open('dados.json', 'w') as file:
-    file.write(json_data)
-
-print(data)
+# print(data)
